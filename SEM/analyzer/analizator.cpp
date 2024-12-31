9a8ad3db115ba6841bfbf452037fa64d42d9168e
Matija Kukic <matija-luka.kukic@fer.hr>
2024-12-31 03:20:52 +0100

    Expressions in semantic analysis done.

--- SEM/analyzer/analizator.cpp
+++ SEM/analyzer/analizator.cpp
@@ -70,12 +70,23 @@ bool checkImp(type t1, type t2) {
     if (t1 == kint) {
         if (t2 == Int)
             return true;
-
     } else if (t1 == kchar) {
         if (t2 == Int || t2 == Char || t2 == kint)
             return true;
     } else if (t1 == Char) {
-        if (t2 == Int)
+        if (t2 == Int) {
+            return true;
+        } else if (t2 == kchar) {
+            return true;
+        }
+    } else if (t1 == Int) {
+        if (t2 == kint)
+            return true;
+    } else if (t1 == arrint) {
+        if (t2 == arrkint)
+            return true;
+    } else if (t1 == arrchar) {
+        if (t2 == arrkchar)
             return true;
     }
     return t1 == t2;
@@ -100,6 +111,9 @@ void prodErr(const shared_ptr<Tree> &node) {
 Type IZRAZ(const shared_ptr<Tree> &node);
 vector<type> LISTA_ARGUMENATA(const shared_ptr<Tree> &node);
 Type CAST_IZRAZ(const shared_ptr<Tree> &node);
+type IME_TIPA(const shared_ptr<Tree> &node);
+type SPECIFIKATOR_TIPA(const shared_ptr<Tree> &node);
+
 void errPrint(string message) {
     string redS = "\033[31m", redE = "\033[0m";
     cerr << redS << message << redE << endl;
@@ -259,10 +273,7 @@ Type POSTFIKS_IZRAZ(const shared_ptr<Tree> &node) {
     }
     return Type(Error, false);
 }
-Type IZRAZ(const shared_ptr<Tree> &node) { return Type(arrint, 2, true); }
-Type IZRAZ_PRIDRUZIVANJA(const shared_ptr<Tree> &node) {
-    return Type(Error, false);
-}
+Type IZRAZ_PRIDRUZIVANJA(const shared_ptr<Tree> &node);
 vector<type> LISTA_ARGUMENATA(const shared_ptr<Tree> &node) {
     const vector<shared_ptr<Tree>> &u = node->children;
     int curr = 0;
@@ -333,6 +344,432 @@ Type UNARNI_IZRAZ(const shared_ptr<Tree> &node) {
     return Type(Error, false);
 }
 
+Type CAST_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    vector<string> v = splitSpaces(ulaz);
+    if (v[0] == "<unarni_izraz>") {
+        return UNARNI_IZRAZ(u[curr]);
+    } else if (ulaz == "L_ZAGRADA") {
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<ime_tipa>") {
+            type t = IME_TIPA(u[curr]);
+            if (!(t == Int || t == kint || t == Char || t == kchar)) {
+                prodErr(node);
+            }
+            curr++;
+            ulaz = u[curr]->node;
+            vector<string> v2 = splitSpaces(ulaz);
+            if (v2[0] != "D_ZAGRADA")
+                errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA CAST_IZRAZ 3");
+            curr++;
+            ulaz = u[curr]->node;
+            if (ulaz == "<cast_izraz>") {
+                Type t2 = CAST_IZRAZ(u[curr]);
+                if (checkImp(t2.type, t) || (t2.type == Int && t == Char)) {
+                    return Type(t, false);
+                } else {
+                    prodErr(node);
+                }
+            } else {
+                errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA CAST_IZRAZ 2");
+            }
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA CAST_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA CAST_IZRAZ 1");
+    }
+    return Type(Error, false);
+}
+
+type IME_TIPA(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    vector<string> v = splitSpaces(ulaz);
+    if (v[0] == "<specifikator_tipa>") {
+        type t = SPECIFIKATOR_TIPA(u[curr]);
+        return t;
+    } else if (v[0] == "KR_CONST") {
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<specifikator_tipa>") {
+            type t = SPECIFIKATOR_TIPA(u[curr]);
+            if (t == Void) {
+                prodErr(node);
+            }
+            if (t == Int) {
+                return kint;
+            } else if (t == Char) {
+                return kchar;
+            } else {
+                prodErr(node);
+            }
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA IME_TIPA 1");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA IME_TIPA 2");
+    }
+    return Error;
+}
+type SPECIFIKATOR_TIPA(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    vector<string> v = splitSpaces(ulaz);
+    if (v[0] == "KR_VOID") {
+        return Void;
+    } else if (v[0] == "KR_CHAR") {
+        return Char;
+    } else if (v[0] == "KR_INT") {
+        return Int;
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA SPECIFIKATOR_TIPA 1");
+    }
+    return Error;
+}
+Type MULTIPLIKATIVNI_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+
+    if (ulaz == "<cast_izraz>") {
+        return CAST_IZRAZ(u[curr]);
+    } else if (ulaz == "<multiplikativni_izraz>") {
+        Type t = MULTIPLIKATIVNI_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_PUTA" || v[0] == "OP_DIJELI" || v[0] == "OP_MOD")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA MULTIPLIKATIVNI_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<cast_izraz>") {
+            Type t2 = CAST_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        }
+    }
+    return Type(Error, false);
+}
+Type ADITIVNI_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<multiplikativni_izraz>") {
+        return MULTIPLIKATIVNI_IZRAZ(u[curr]);
+    } else if (ulaz == "<aditivni_izraz>") {
+        Type t = ADITIVNI_IZRAZ(u[curr]);
+        curr++;
+        ulaz = u[curr]->node;
+        if (!checkImp(t.type, Int)) {
+            prodErr(node);
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "PLUS" || v[0] == "MINUS")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA ADITIVNI_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<multiplikativni_izraz>") {
+            Type t2 = MULTIPLIKATIVNI_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA ADITIVNI_IZRAZ 2");
+    }
+    return Type(Error, false);
+}
+
+Type ODNOSNI_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<aditivni_izraz>") {
+        return ADITIVNI_IZRAZ(u[curr]);
+    } else if (ulaz == "<odnosni_izraz>") {
+        Type t = ODNOSNI_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_LT" || v[0] == "OP_GT" || v[0] == "OP_LTE" ||
+              v[0] == "OP_GTE")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA ODNOSNI_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<aditivni_izraz>") {
+            Type t2 = ADITIVNI_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA ODNOSNI_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA ODNOSNI_IZRAZ 3");
+    }
+    return Type(Error, false);
+}
+
+Type JEDNAKOSNI_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<odnosni_izraz>") {
+        return ODNOSNI_IZRAZ(u[curr]);
+    } else if (ulaz == "<jednakosni_izraz>") {
+        Type t = JEDNAKOSNI_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_EQ" || v[0] == "OP_NEQ")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA JEDNAKOSNI_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<odnosni_izraz>") {
+            Type t2 = ODNOSNI_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA JEDNAKOSNI_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA JEDNAKOSNI_IZRAZ 3");
+    }
+    return Type(Error, false);
+}
+Type BIN_I_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<jednakosni_izraz>") {
+        return JEDNAKOSNI_IZRAZ(u[curr]);
+    } else if (ulaz == "<bin_i_izraz>") {
+        Type t = BIN_I_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_BIN_I")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA BIN_I_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<jednakosni_izraz>") {
+            Type t2 = JEDNAKOSNI_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA  BIN_I_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA BIN_I_IZRAZ 3");
+    }
+    return Type(Error, false);
+}
+Type BIN_XILI_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<bin_i_izraz>") {
+        return BIN_I_IZRAZ(u[curr]);
+    } else if (ulaz == "<bin_xili_izraz>") {
+        Type t = BIN_XILI_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_BIN_XILI")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA BIN_XILI_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<bin_i_izraz>") {
+            Type t2 = BIN_I_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA  BIN_XILI_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA BIN_XILI_IZRAZ 3");
+    }
+    return Type(Error, false);
+}
+Type BIN_ILI_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<bin_xili_izraz>") {
+        return BIN_XILI_IZRAZ(u[curr]);
+    } else if (ulaz == "<bin_ili_izraz>") {
+        Type t = BIN_ILI_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_BIN_ILI")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA BIN_ILI_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<bin_xili_izraz>") {
+            Type t2 = BIN_XILI_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA  BIN_ILI_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA BIN_ILI_IZRAZ 3");
+    }
+    return Type(Error, false);
+}
+Type LOG_I_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<bin_ili_izraz>") {
+        return BIN_ILI_IZRAZ(u[curr]);
+    } else if (ulaz == "<log_i_izraz>") {
+        Type t = LOG_I_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_I")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA LOG_I_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<bin_ili_izraz>") {
+            Type t2 = BIN_ILI_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA  LOG_I_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA LOG_I_IZRAZ 3");
+    }
+    return Type(Error, false);
+}
+Type LOG_ILI_IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<log_i_izraz>") {
+        return LOG_I_IZRAZ(u[curr]);
+    } else if (ulaz == "<log_ili_izraz>") {
+        Type t = LOG_ILI_IZRAZ(u[curr]);
+        if (!checkImp(t.type, Int))
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_ILI")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA LOG_ILI_IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<log_i_izraz>") {
+            Type t2 = LOG_I_IZRAZ(u[curr]);
+            if (!checkImp(t2.type, Int))
+                prodErr(node);
+            return Type(Int, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA  LOG_ILI_IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA LOG_ILI_IZRAZ 3");
+    }
+    return Type(Error, false);
+}
+Type IZRAZ_PRIDRUZIVANJA(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<log_ili_izraz>") {
+        return LOG_ILI_IZRAZ(u[curr]);
+    } else if (ulaz == "<postfiks_izraz>") {
+        Type t = POSTFIKS_IZRAZ(u[curr]);
+        if (t.lexp != 1)
+            prodErr(node);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (!(v[0] == "OP_PRIDRUZI")) {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA IZRAZ_PRIDRUZIVANJA 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<izraz_pridruzivanja>") {
+            Type t2 = IZRAZ_PRIDRUZIVANJA(u[curr]);
+            if (!checkImp(t2.type, t.type))
+                prodErr(node);
+            return Type(t.type, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA  IZRAZ_PRIDRUZIVANJA 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA IZRAZ_PRIDRUZIVANJA 3");
+    }
+    return Type(Error, false);
+}
+Type IZRAZ(const shared_ptr<Tree> &node) {
+    const vector<shared_ptr<Tree>> &u = node->children;
+    int curr = 0;
+    string ulaz = u[curr]->node;
+    if (ulaz == "<izraz_pridruzivanja>") {
+        return IZRAZ_PRIDRUZIVANJA(u[curr]);
+    } else if (ulaz == "<izraz>") {
+        Type t = IZRAZ(u[curr]);
+        curr++;
+        ulaz = u[curr]->node;
+        vector<string> v = splitSpaces(ulaz);
+        if (v[0] != "ZAREZ") {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA IZRAZ 1");
+        }
+        curr++;
+        ulaz = u[curr]->node;
+        if (ulaz == "<izraz_pridruzivanja>") {
+            Type t2 = IZRAZ_PRIDRUZIVANJA(u[curr]);
+            return Type(t2.type, false);
+        } else {
+            errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA IZRAZ 2");
+        }
+    } else {
+        errPrint("KRIVI ULAZNI ZNAKOVI GRAMATIKA IZRAZ 3");
+    }
+    return Type(Error, false);
+}
 Type test1() { return Type(Void, 2, 1); }
 Type test() { return test1(); }
 
